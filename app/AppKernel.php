e779145ffc0c6be2aaeb456777d10d9ab7f798c5
Kessler <kessler.dev@gmail.com>
2014-03-08 20:32:54 +0100

    user-management 0.9

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -20,6 +20,7 @@ class AppKernel extends Kernel
             new JMS\SerializerBundle\JMSSerializerBundle($this),
             new FOS\RestBundle\FOSRestBundle(),
             new Skimia\AngularBundle\SkimiaAngularBundle(),
+            new FOS\UserBundle\FOSUserBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
